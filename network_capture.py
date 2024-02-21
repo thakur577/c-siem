d05e74a36ca3f0c1098e08997e0a1b68f77c6adb
thakur577 <39896213+thakur577@users.noreply.github.com>
2024-02-21 11:42:59 +0530

    Added few more protocols
    Added few more protocols

--- network_capture.py
+++ network_capture.py
@@ -1,3 +1,8 @@
+"""
+TODO: I need to send all packets/protocols to syslog server as discussed with Chetan
+need to design log format to be sent
+"""
+
 import psutil
 import pyshark
 import pytz
@@ -74,7 +79,7 @@ def get_available_interfaces():
 def process_packet(packet):
     print("---------------------------------------------------------------------------------------------------------------------------")
     print("packet.layers: ", packet.layers)
-    print("packet.length: ", packet.length)
+    #print("packet.length: ", packet.length)
     #print("packet._packet_string: ", packet._packet_string)
     #print("packet.frame_info: ", packet.frame_info)
     #print("packet.captured_length: ", packet.captured_length)
@@ -82,15 +87,118 @@ def process_packet(packet):
     #print("packet.interface_captured: ", packet.interface_captured)
     #print("packet.show: ", packet.show)
     #print("packet.number: ", packet.number)
-    print("packet.sniff_time: ", packet.sniff_time)
+    #print("packet.sniff_time: ", packet.sniff_time)
     #print("packet.sniff_timestamp: ", packet.sniff_timestamp)
+    print("packet.pretty_print():"+str(packet.pretty_print()))
+    #print("dir function on ipv6 packet: ", dir(packet))
 
     # print("packet start :--------------------------------------------------------------------------------------------------")
     # print("packet string: ",str(packet))
     # print("packet end :--------------------------------------------------------------------------------------------------")
+
+    if hasattr(packet, 'ipv6') and hasattr(packet.ipv6, 'src') and hasattr(packet.ipv6, 'dst'):
+        print("ipv6 packet: ",str(packet.ipv6))
+        #print("dir function on ipv6 packet: ", dir(packet.ipv6))
+        #print("packet.ipv6.addr: ", str(packet.ipv6.addr))
+        #print("packet.ipv6.dst: ", str(packet.ipv6.dst))
+        #print("packet.ipv6.dst_host: ", str(packet.ipv6.dst_host))
+        #print("packet.ipv6.field_names: ", str(packet.ipv6.field_names))
+        #print("packet.ipv6.flow: ", str(packet.ipv6.flow))
+        #print("packet.ipv6.host: ", str(packet.ipv6.host))
+        #print("packet.ipv6.ip_version: ", str(packet.ipv6.ip_version))
+        #print("packet.ipv6.src: ", str(packet.ipv6.src))
+        #print("packet.ipv6.src_host: ", str(packet.ipv6.src_host))
+        #print("packet.ipv6.tclass: ", str(packet.ipv6.tclass))
+        #print("packet.ipv6.tclass_dscp: ", str(packet.ipv6.tclass_dscp))
+        #print("packet.ipv6.tclass_ecn: ", str(packet.ipv6.tclass_ecn))
+        src_ip = packet.ipv6.src
+        dst_ip = packet.ipv6.dst
+        # Access the IP layer
+        ipv6_layer = packet['IPv6']
+        # Access the 'len' field in the IP layer, which represents the total length of the IP packet
+        total_length = int(ipv6_layer.plen)
+        print(f"IPv6 Total Length: {total_length}")
+
+        # extract ICMPV6 Details in IPv6 Packet
+        if hasattr(packet, 'icmpv6'):
+            icmpv6_packet = ""
+            protocol = "icmpv6"
+            timestamp = packet.sniff_time
+            print("timestamp field in IPv6-ICMPv6 packet:" + str(timestamp))
+            # Check if the packet has an ICMPV6 layer
+            if 'ICMPV6' in packet:
+                # Access the ICMPV6 layer
+                icmpv6_packet = str(packet.icmpv6)
+                print("'icmpv6_packet' under IPv6 Layer")
+
+                icmpv6_json = {
+                    "src_ip": src_ip,
+                    "dst_ip": dst_ip,
+                    "protocol": protocol,
+                    "timestamp": str(timestamp),
+                    "packet_length": total_length,
+                    "icmpv6_packet": icmpv6_packet
+                }
+                # send json over tcp
+                send_json_over_tcp(tcp_host, tcp_port, icmpv6_json)
+
+
+        # extract UDP Details in IPv6 Packet
+        if hasattr(packet, 'udp') and hasattr(packet.udp, 'srcport') and hasattr(packet.udp, 'dstport'):
+            print("UDP Packet in IPv6 Layer")
+            src_port = packet.udp.srcport
+            dst_port = packet.udp.dstport
+            type_of_service = ""
+            num_bytes = ""
+            num_packets = ""
+            protocol_type = "udp"
+            timestamp = packet.sniff_time
+            print("timestamp field in IPv6 UDP packet:"+str(timestamp))
+
+            dhcpv6_packet = ""
+            mdns_packet = ""
+            llmnr_packet = ""
+
+            # Check if the packet has an DHCPV6 layer
+            if 'DHCPV6' in packet:
+                # Access the DHCPV6 layer
+                dhcpv6_packet = str(packet.dhcpv6)
+                print("'dhcpv6_packet' under UDP Layer")
+            # Check if the packet has an MDNS layer
+            if 'MDNS' in packet:
+                # Access the MDNS layer
+                mdns_packet = str(packet.mdns)
+                print("'mdns_packet' under UDP Layer")
+            # Check if the packet has an LLMNR layer
+            if 'LLMNR' in packet:
+                # Access the LLMNR layer
+                llmnr_packet = str(packet.llmnr)
+                print("'llmnr_packet' under UDP Layer")
+
+
+            udp_json = {
+                "src_ip": src_ip,
+                "dst_ip": dst_ip,
+                "src_port": src_port,
+                "dst_port": dst_port,
+                "protocol": protocol_type,
+                "timestamp": str(timestamp),
+                "packet_length": total_length,
+                "dhcpv6_packet": dhcpv6_packet,
+                "mdns_packet" : mdns_packet,
+                "llmnr_packet" : llmnr_packet
+            }
+            # send json over tcp
+            send_json_over_tcp(tcp_host, tcp_port, udp_json)
+
+        if hasattr(packet, 'tcp') and hasattr(packet.tcp, 'srcport') and hasattr(packet.tcp, 'dstport'):
+            print("TCP Packet in IPv6 Layer")
+
+
     if hasattr(packet, 'ip') and hasattr(packet.ip, 'src') and hasattr(packet.ip, 'dst'):
-        #print(""" if hasattr(packet, 'ip') and hasattr(packet.ip, 'src') and hasattr(packet.ip, 'dst') """)
+        # source ip
         src_ip = packet.ip.src
+        # destination ip
         dst_ip = packet.ip.dst
         # Access the IP layer
         ip_layer = packet['IP']
@@ -99,10 +207,8 @@ def process_packet(packet):
         print(f"IP Total Length: {total_length}")
 
         if hasattr(packet, 'udp') and hasattr(packet.udp, 'srcport') and hasattr(packet.udp, 'dstport'):
-            #print(""" if hasattr(packet, 'udp') and hasattr(packet.udp, 'srcport') and hasattr(packet.udp, 'dstport'): """)
             src_port = packet.udp.srcport
             dst_port = packet.udp.dstport
-
             type_of_service = ""
             num_bytes = ""
             num_packets = ""
@@ -121,7 +227,52 @@ def process_packet(packet):
                 timestamp = int(float(timestamp))
                 timestamp = convert_utc_to_ist(timestamp)
 
-            print(f"Source IP: {src_ip}, Destination IP: {dst_ip}, Source Port: {src_port}, Destination Port: {dst_port},Layer 3 Protocol Type: {protocol_type},Type of Service (ToS): {type_of_service},Ingress Interface: {packet.interface_captured},Timestamp: {timestamp},Number of Bytes: {num_bytes}, Number of Packets: {num_packets}")
+            #print(f"Source IP: {src_ip}, Destination IP: {dst_ip}, Source Port: {src_port}, Destination Port: {dst_port},Layer 3 Protocol Type: {protocol_type},Type of Service (ToS): {type_of_service},Ingress Interface: {packet.interface_captured},Timestamp: {timestamp},Number of Bytes: {num_bytes}, Number of Packets: {num_packets}")
+
+            ssdp_packet = ""
+            mdns_packet = ""
+            dhcp_packet = ""
+            nbns_packet = ""
+            smb_packet = ""
+            dns_packet = ""
+            llmnr_packet = ""
+            # Check if the packet has a SSDP layer
+            if 'SSDP' in packet:
+                # Access the SSDP layer
+                ssdp_packet = str(packet.ssdp)
+                print("'ssdp_packet' under UDP Layer")
+            # Check if the packet has an MDNS layer
+            if 'MDNS' in packet:
+                # Access the MDNS layer
+                mdns_packet = str(packet.mdns)
+                print("'mdns_packet' under UDP Layer")
+            # Check if the packet has an DHCP layer
+            if 'DHCP' in packet:
+                # Access the DHCP layer
+                dhcp_packet = str(packet.dhcp)
+                print("'dhcp_packet' under UDP Layer")
+            # Check if the packet has an NBNS layer
+            if 'NBNS' in packet:
+                # Access the NBNS layer
+                nbns_packet = str(packet.nbns)
+                print("'nbns_packet' under UDP Layer")
+            # Check if the packet has an SMB layer
+            if 'SMB' in packet:
+                # Access the SMB layer
+                smb_packet = str(packet.smb)
+                print("'smb_packet' under UDP Layer")
+            # Check if the packet has a DNS layer
+            if 'DNS' in packet:
+                # Access the DNS layer
+                dns_packet = str(packet.dns)
+                print("'dns_packet' under UDP Layer")
+            # Check if the packet has an LLMNR layer
+            if 'LLMNR' in packet:
+                # Access the LLMNR layer
+                llmnr_packet = str(packet.llmnr)
+                print("'llmnr_packet' under UDP Layer")
+
+
             udp_json = {
                 "src_ip":src_ip,
                 "dst_ip": dst_ip,
@@ -129,7 +280,14 @@ def process_packet(packet):
                 "dst_port": dst_port,
                 "protocol" : protocol_type,
                 "timestamp": str(timestamp),
-                "packet_length": total_length
+                "packet_length": total_length,
+                "ssdp_packet": ssdp_packet,
+                "mdns_packet": mdns_packet,
+                "dhcp_packet" : dhcp_packet,
+                "nbns_packet" : nbns_packet,
+                "smb_packet" : smb_packet,
+                "dns_packet" : dns_packet,
+                "llmnr_packet" : llmnr_packet
             }
             # send json over tcp
             send_json_over_tcp(tcp_host,tcp_port,udp_json)
@@ -152,21 +310,21 @@ def process_packet(packet):
             # Access the 'flags' field in the TCP layer, which represents the TCP control flags
             flags_value = int(tcp_layer.flags, 16)  # Convert the hexadecimal value to an integer
             print("TCP Flag Value:"+str(flags_value))
+            tcp_flags = ""
 
             # Define TCP flag values
             urg = (flags_value & 0x20) >> 5
-            print("TCP Flag urg:" + str(urg))
+            #print("TCP Flag urg:" + str(urg))
             ack = (flags_value & 0x10) >> 4
-            print("TCP Flag ack:" + str(ack))
+            #print("TCP Flag ack:" + str(ack))
             psh = (flags_value & 0x08) >> 3
-            print("TCP Flag psh:" + str(psh))
+            #print("TCP Flag psh:" + str(psh))
             rst = (flags_value & 0x04) >> 2
-            print("TCP Flag rst:" + str(rst))
+            #print("TCP Flag rst:" + str(rst))
             syn = (flags_value & 0x02) >> 1
-            print("TCP Flag syn:" + str(syn))
+            #print("TCP Flag syn:" + str(syn))
             fin = flags_value & 0x01
-            print("TCP Flag fin:" + str(fin))
-
+            #print("TCP Flag fin:" + str(fin))
 
             num_bytes = ""
             num_packets = ""
@@ -185,7 +343,7 @@ def process_packet(packet):
                 timestamp = int(float(timestamp))
                 timestamp = convert_utc_to_ist(timestamp)
 
-            print(f"Source IP: {src_ip}, Destination IP: {dst_ip}, Source Port: {src_port}, Destination Port: {dst_port},Layer 3 Protocol Type: {protocol_type},Type of Service (ToS): {type_of_service},Ingress Interface: {packet.interface_captured},Timestamp: {timestamp},Number of Bytes: {num_bytes}, Number of Packets: {num_packets}")
+            #print(f"Source IP: {src_ip}, Destination IP: {dst_ip}, Source Port: {src_port}, Destination Port: {dst_port},Layer 3 Protocol Type: {protocol_type},Type of Service (ToS): {type_of_service},Ingress Interface: {packet.interface_captured},Timestamp: {timestamp},Number of Bytes: {num_bytes}, Number of Packets: {num_packets}")
             tcp_json = {
                 "src_ip":src_ip,
                 "dst_ip": dst_ip,
@@ -440,6 +598,7 @@ print("Available network interfaces:", available_interfaces)
 if available_interfaces:
     # Specify the network interface to capture traffic from
     network_interface = available_interfaces[2]  # Choose the appropriate interface from the list
+    print("network_interface: "+str(network_interface))
     # Capture NetFlow traffic
     capture = pyshark.LiveCapture(interface=network_interface)
     # Set a callback function to process each captured packet
@@ -594,3 +753,85 @@ dir(packet.udp) - dir function on packer.udp
 'time_delta', 
 'time_relative']
 """
+"""
+dir function on ipv6 packet
+['_layer_name',
+'_pretty_print_layer_fields',
+'_sanitize_field_name', 
+'addr', 
+'dst', 
+'dst_host', 
+'field_names', 
+'flow', 
+'get', 
+'get_field', 
+'get_field_by_showname', 
+'get_field_value', 
+'has_field', 
+'hlim', 
+'host', 
+'ip_version', 
+'layer_name', 
+'nxt', 
+'plen', 
+'pretty_print', 
+'raw_mode', 
+'src', 
+'src_host', 
+'tclass', 
+'tclass_dscp', 
+'tclass_ecn', 
+'version']
+"""
+
+"""
+NBNS (NetBIOS Name Service) packets come under the UDP (User Datagram Protocol) layer. NetBIOS Name Service is a protocol used in Windows networks for name resolution, converting NetBIOS names to IP addresses.
+"""
+
+"""
+SSDP (Simple Service Discovery Protocol) packets come under the UDP (User Datagram Protocol) layer. SSDP is a network protocol used for discovering network services and presence information on a local network.
+"""
+
+"""
+The NBDGM (NetBIOS Datagram Service) packets typically occur over UDP (User Datagram Protocol). The NetBIOS Datagram Service is used for broadcasting messages on a LAN (Local Area Network) for NetBIOS name resolution and other NetBIOS-related activities.
+"""
+
+"""
+BJNP (Bonjour Network Printer) is a protocol used for discovering and configuring printers over a network using Apple's Bonjour (also known as Zeroconf) protocol suite. In PyShark, you would typically find BJNP packets as part of the UDP (User Datagram Protocol) layer.
+"""
+
+"""
+LLMNR stands for Link-Local Multicast Name Resolution. It is a protocol used for name resolution in local networks when DNS (Domain Name System) resolution fails. LLMNR allows hosts to resolve the IP addresses of neighboring devices without the need for a DNS server.
+When a device needs to resolve the IP address of another device on the same local network, it sends an LLMNR query packet to the multicast address 224.0.0.252 on port 5355. Other devices on the network that recognize their own name in the query respond with their IP address.
+LLMNR operates similarly to the more widely used mDNS (Multicast DNS) protocol, but they serve slightly different purposes. LLMNR is primarily used in Windows environments, while mDNS is more common in macOS and Linux environments.
+In packet captures or network analysis tools like Wireshark or PyShark, LLMNR packets are typically visible as UDP packets with destination port 5355. These packets contain LLMNR-specific data used for name resolution within the local network.
+"""
+
+"""
+[<ETH Layer>, <IP Layer>, <UDP Layer>, <NBDGM Layer>, <SMB Layer>, <MAILSLOT Layer>, <BROWSER Layer>]
+[<ETH Layer>, <IP Layer>, <UDP Layer>, <SSDP Layer>]
+[<ETH Layer>, <IP Layer>, <UDP Layer>, <BJNP Layer>]
+[<ETH Layer>, <IPV6 Layer>, <UDP Layer>, <BJNP Layer>]
+[<ETH Layer>, <IPV6 Layer>, <UDP Layer>, <DATA Layer>]
+[<ETH Layer>, <IP Layer>, <UDP Layer>, <DATA Layer>]
+[<ETH Layer>, <IP Layer>, <UDP Layer>, <MDNS Layer>]
+[<ETH Layer>, <IPV6 Layer>, <UDP Layer>, <MDNS Layer>]
+[<ETH Layer>, <IP Layer>, <UDP Layer>, <DHCP Layer>]
+[<ETH Layer>, <IPV6 Layer>, <UDP Layer>, <DHCPV6 Layer>]
+[<ETH Layer>, <IP Layer>, <UDP Layer>, <NBNS Layer>]
+[<ETH Layer>, <IP Layer>, <UDP Layer>, <DNS Layer>]
+[<ETH Layer>, <IP Layer>, <UDP Layer>, <LLMNR Layer>]
+[<ETH Layer>, <IPV6 Layer>, <UDP Layer>, <LLMNR Layer>]
+[<ETH Layer>, <LLC Layer>, <STP Layer>]
+[<ETH Layer>, <IP Layer>, <VRRP Layer>]
+[<ETH Layer>, <ARP Layer>]
+[<ETH Layer>, <DATA Layer>]
+[<ETH Layer>, <IP Layer>, <TCP Layer>, <DATA Layer>]
+[<ETH Layer>, <IP Layer>, <TCP Layer>]
+[<ETH Layer>, <LLDP Layer>]
+[<ETH Layer>, <IP Layer>, <IGMP Layer>]
+[<ETH Layer>, <IPV6 Layer>, <ICMPV6 Layer>]
+[<ETH Layer>, <IP Layer>, <TCP Layer>, <TLS Layer>]
+[<ETH Layer>, <IP Layer>, <TCP Layer>, <HTTP Layer>, <JSON Layer>]
+"""
+
